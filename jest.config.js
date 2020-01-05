1410292fa626663d043838860fded9a13bd55a4b
Sarratas <sarratas@gmail.com>
2020-01-05 01:32:30 +0100

    Update tests config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  collectCoverageFrom : ["src/**/*.ts", "!src/index.ts", "!src/tests/*.ts"],
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    collectCoverageFrom : ["src/**/*.ts", "!src/index.ts", "!src/tests/**/*.ts"],
 };
