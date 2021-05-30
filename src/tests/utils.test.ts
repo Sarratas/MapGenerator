0eaaab9f48b5c8125d0ffcae958cfa06ae71618d
Sarratas <sarratas@gmail.com>
2021-05-30 11:37:01 +0200

    Update jest version

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -1,50 +1,46 @@
 import Utils from '../utils';
 
 test('Utils.range', () => {
-    expect(Utils.range(0, 0)).toEqual([0]);
-    expect(Utils.range(0, 1)).toEqual([0, 1]);
-    expect(Utils.range(2, 4)).toEqual([2, 3, 4]);
-    expect(Utils.range(-2, 2)).toEqual([-2, -1, 0, 1, 2]);
+  expect(Utils.range(0, 0)).toEqual([0]);
+  expect(Utils.range(0, 1)).toEqual([0, 1]);
+  expect(Utils.range(2, 4)).toEqual([2, 3, 4]);
+  expect(Utils.range(-2, 2)).toEqual([-2, -1, 0, 1, 2]);
 
-    expect(Utils.range(1, 0)).toEqual([]);
+  expect(Utils.range(1, 0)).toEqual([]);
 });
 
 test('Utils.throttle without this', () => {
-    jest.useFakeTimers();
+  jest.useFakeTimers();
 
-    const fn = jest.fn();
-    const throttledFunc = Utils.throttle(fn, 1000);
-    
-    throttledFunc('a');
-    throttledFunc('a');
-    throttledFunc('a');
+  const fn = jest.fn();
+  const throttledFunc = Utils.throttle(fn, 1000);
 
-    expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
-    expect(fn).toBeCalledTimes(1);
+  throttledFunc('a');
+  throttledFunc('a');
+  throttledFunc('a');
 
-    jest.runAllTimers();
+  expect(fn).toBeCalledTimes(1);
 
-    expect(fn).toBeCalledTimes(2);
+  jest.runAllTimers();
+
+  expect(fn).toBeCalledTimes(2);
 });
 
 test('Utils.throttle with this', () => {
-    jest.useFakeTimers();
+  jest.useFakeTimers();
 
-    const obj = {
-        fn: jest.fn()
-    }
-    const throttledFunc = Utils.throttle(obj.fn, 1000);
-    
-    throttledFunc();
-    throttledFunc();
-    throttledFunc();
+  const obj = {
+    fn: jest.fn(),
+  };
+  const throttledFunc = Utils.throttle(obj.fn, 1000);
 
-    expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
-    expect(obj.fn).toBeCalledTimes(1);
+  throttledFunc();
+  throttledFunc();
+  throttledFunc();
 
-    jest.runAllTimers();
+  expect(obj.fn).toBeCalledTimes(1);
 
-    expect(obj.fn).toBeCalledTimes(2);
+  jest.runAllTimers();
+
+  expect(obj.fn).toBeCalledTimes(2);
 });
